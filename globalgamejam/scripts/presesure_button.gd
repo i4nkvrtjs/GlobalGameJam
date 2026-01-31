6e85f5a678e412a73846bc88e7ba7c10ae0a48de
Vonrov <franciscorovito@gmail.com>
2026-01-31 15:31:55 -0300

    Botones

--- globalgamejam/scripts/presesure_button.gd
+++ globalgamejam/scripts/presesure_button.gd
@@ -2,7 +2,6 @@ extends StaticBody3D
 
 @export var press_depth := 0.05
 @export var press_time := 0.2
-@onready var animationbutton = $"../Pedestal_Boton_Futuro/AnimationPlayer"
 
 var pressed := false
 var start_position: Vector3
@@ -17,16 +16,10 @@ func interact(_interactor):
 		return
 
 	pressed = true
-	animationbutton.play("Cube_001Action")
 	_press_animation()
 
 
 func _press_animation():
-	var tween = create_tween()
-	tween.tween_property(
-		self,
-		"global_position",
-		start_position - Vector3.DOWN * press_depth,
-		press_time
-	)
-	
+	var BotonAnimado = get_child(2)
+	var AnimaciondelBoton = BotonAnimado.get_child(1)
+	AnimaciondelBoton.play("Cube_001Action_001")
